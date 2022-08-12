d1268998fd3ef64c43b1ad4f2e3d60f8f78e87aa
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-08-12 15:34:17 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'normalize.css';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <p>Hello World</p>
+  </React.StrictMode>
+);
