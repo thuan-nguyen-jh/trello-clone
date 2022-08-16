fb206ccd4e27a07fd70bc782b059ce6d3c994f7b
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-08-16 11:04:35 +0700

    create route to navigate to login and sign up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Authentication from './pages/Authentication/Authentication';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Endpoint from './data/Endpoint';
 
 import 'normalize.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <p>Hello World</p>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Switch>
+      <Route path={[Endpoint.login, Endpoint.signUp]} component={Authentication} />
+    </Switch>
+  </BrowserRouter>
 );
