24efe4dcc7f1970d3f253131fd11cb54ca63593b
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-08-19 13:51:16 +0700

    add topbar component

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { logout } from "../../utils/firebase";
+
+import './Topbar.css';
+
+export default class Topbar extends React.Component {
+  render() {
+    return (
+      <div className="top-bar">
+        <button className="top-bar__logout-btn" onClick={logout}>
+          Logout
+        </button>
+      </div>
+    );
+  }
+}
