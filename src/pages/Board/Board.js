6ea2f3e853fe46a647c54e4184130301e925d7bb
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-08-21 02:59:10 +0700

    add Board component

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,69 @@
+import React from "react";
+import CreateComponentButton from "../../components/Buttons/CreateComponentButton/CreateComponentButton";
+import Column from "../../components/Column/Column";
+import Topbar from "../../components/Topbar/Topbar";
+import { createNewColumn, getUserBoard } from "../../utils/firebase";
+
+import "./Board.css";
+
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardRef: undefined,
+      columns: [],
+    };
+
+    this.handleCreateColumn = this.handleCreateColumn.bind(this);
+  }
+
+  componentDidMount() {
+    const { currentUser } = this.props;
+    getUserBoard(currentUser.uid).then(snapshot => {
+      const boardData = snapshot.data();
+      this.setState({
+        boardRef: snapshot.ref,
+        columns: boardData.columns,
+      });
+    });
+  }
+
+  async handleCreateColumn(name) {
+    if (name === "") {
+      return false;
+    }
+
+    const columnRef = await createNewColumn(this.state.boardRef, name);
+    this.setState({
+      columns: [...this.state.columns, columnRef],
+    });
+  }
+
+  render() {
+    const { boardRef, columns } = this.state;
+    if (boardRef === undefined) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="board-container">
+        <Topbar />
+        <div className="columns-container">
+          {columns.map(columnRef => (
+            <Column
+              key={columnRef._key.path.segments.at(-1)}
+              columnRef={columnRef}
+            />
+          ))}
+          <div className="column">
+            <CreateComponentButton
+              wrapperClassName="column-header"
+              onCreate={this.handleCreateColumn}
+              placeholder="+ Add a new column"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
