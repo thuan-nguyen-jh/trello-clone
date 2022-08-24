4d7e432ffdc53a970c8709efdcc140a16b6e7a27
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-08-24 14:40:20 +0700

    add move card and load card to Column component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import EditableHeader from '../EditableHeader/EditableHeader';
+import CreateComponentButton from '../Buttons/CreateComponentButton/CreateComponentButton';
+import Card from '../Card/Card';
 import { editColumnName, getDoc } from '../../utils/db';
 
 import './Column.css';
 
 export default function Column(props) {
-  const { columnRef } = props;
+  const { columnRef, index: columnIndex, columnCount, cards, onCreateCard, onMoveCard } = props;
   const [title, setTitle] = useState("");
 
   useEffect(() => {
@@ -25,6 +27,18 @@ export default function Column(props) {
     return true;
   }
 
+  function createCard(title) {
+    return onCreateCard(columnIndex, title);
+  }
+
+  function moveCardToLeft(cardIndex) {
+    return onMoveCard(cardIndex, columnIndex, columnIndex - 1);
+  }
+
+  function moveCardToRight(cardIndex) {
+    return onMoveCard(cardIndex, columnIndex, columnIndex + 1);
+  }
+
   return (
     <div className='column'>
       <div className='column-header'>
@@ -34,6 +48,32 @@ export default function Column(props) {
         />
       </div>
       <div className='column-content'>
+        {
+          cards.map((card, cardIndex) => {
+            const { ref, data } = card;
+            const isAtFirstColumn = columnIndex === 0;
+            const isAtLastColumn = columnIndex === columnCount - 1;
+            return (
+              <Card
+                key={ref.id}
+                cardRef={ref}
+                cardIndex={cardIndex}
+                columnIndex={columnIndex}
+                title={data.title}
+                content={data.content}
+                onMoveCardToLeft={moveCardToLeft}
+                onMoveCardToRight={moveCardToRight}
+                isAtFirstColumn={isAtFirstColumn}
+                isAtLastColumn={isAtLastColumn}
+              />);
+          })
+        }
+        <div className='card'>
+          <CreateComponentButton
+            onCreate={createCard}
+            placeholder='+ Add a card'
+          />
+        </div>
       </div>
     </div>
   );
